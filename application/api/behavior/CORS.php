f9cd68cc0b7141af489a44e7faeb061706faca70
l123123ianxiao <296856176@qq.com>
2019-07-30 18:32:19 +0800

    add

--- application/api/behavior/CORS.php
+++ application/api/behavior/CORS.php
@@ -14,7 +14,7 @@ class CORS
 	public function appInit($params){
 		header('Access-Control-Allow-Origin:*');
 		header("Access-Control-Allow-Headers: token,Origin,X-Requested-With,Content-Type,Accept");
-		header('Access-Control-Allow-Methods:POST,GET,DELETE');
+		header('Access-Control-Allow-Methods:POST,GET,DELETE,PUT');
 		if(request()->isOptions()){
 			exit();
 		}
